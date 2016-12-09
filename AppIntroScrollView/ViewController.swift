0a4641a8d16cdc6c0e515928acc831ba5f0c254b
Sobhan-Eskandari <arash1@mail.usf.edu>
2016-12-09 12:56:28 +0330

    Added Images next to each other

--- AppIntroScrollView/ViewController.swift
+++ AppIntroScrollView/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    let imageNames = ["introtour01","introtour02","introtour03","introtour04"]
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setUpImageViews()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    func setUpImageViews(){
+        var totalWidth: CGFloat = 0
+        
+        for imageName in imageNames {
+            let image = UIImage(named: imageName)
+            let imageView = UIImageView(image: image)
+            imageView.frame = CGRect(origin: CGPoint(x:totalWidth,y:0), size: view.bounds.size)
+            imageView.contentMode = .scaleAspectFit
+            scrollView.addSubview(imageView)
+            totalWidth += imageView.bounds.size.width
+        }
+        
+        scrollView.contentSize = CGSize(width: totalWidth, height: scrollView.bounds.size.height)
+    }
 
 }
 
